fbbe655ed2c2590aca99289feb7f8d62bd22d6aa
TheRiddler200 <46275954+TheRiddler200@users.noreply.github.com>
2023-07-29 02:31:13 -0500

    quick update
    updated function

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+
+int main() {
+
+
+    std::string name;
+    std::getline(std::cin, name);
+
+    std::cout<<"Hello, " << name <<std::endl;
+
+    std::cout<<"hello git";
+
+
+
+
+    return 0;   
+}
